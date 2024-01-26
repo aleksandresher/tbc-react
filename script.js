8610170d7790c3102ba883976638c717850b9dce
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2024-01-27 00:00:19 +0400

    main-image-wrapper update

--- script.js
+++ script.js
@@ -9,33 +9,33 @@ toggleBtn.addEventListener("click", () => {
   }
 });
 
-const leftArrow = document.getElementById("left-arrow");
-const rightArrow = document.getElementById("right-arrow");
-const slideWrapper = document.querySelector(".content_inner_slider");
+// const leftArrow = document.getElementById("left-arrow");
+// const rightArrow = document.getElementById("right-arrow");
+// const slideWrapper = document.querySelector(".content_inner_slider");
 
-const slides = document.getElementsByClassName("slide");
-const totalSlides = slides.length;
-console.log(totalSlides);
-let currentSlide = 0;
+// const slides = document.getElementsByClassName("slide");
+// const totalSlides = slides.length;
+// console.log(totalSlides);
+// let currentSlide = 0;
 
-function updateSlidePosition() {
-  for (let i = 0; i < totalSlides; i++) {
-    slides[i].style.transform = `translateX(-${currentSlide * 100}%)`;
-  }
-}
-leftArrow.addEventListener("click", () => {
-  if (currentSlide > 0) {
-    currentSlide--;
-    updateSlidePosition();
-  }
-});
+// function updateSlidePosition() {
+//   for (let i = 0; i < totalSlides; i++) {
+//     slides[i].style.transform = `translateX(-${currentSlide * 100}%)`;
+//   }
+// }
+// leftArrow.addEventListener("click", () => {
+//   if (currentSlide > 0) {
+//     currentSlide--;
+//     updateSlidePosition();
+//   }
+// });
 
-rightArrow.addEventListener("click", () => {
-  if (currentSlide < totalSlides - 1) {
-    currentSlide++;
-    updateSlidePosition();
-  }
-});
+// rightArrow.addEventListener("click", () => {
+//   if (currentSlide < totalSlides - 1) {
+//     currentSlide++;
+//     updateSlidePosition();
+//   }
+// });
 
 const faqs = document.querySelectorAll(".faq");
 
