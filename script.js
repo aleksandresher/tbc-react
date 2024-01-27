74e5a1d026dd8b396666721646f4142f255b2818
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2024-01-27 23:28:22 +0400

    slider done

--- script.js
+++ script.js
@@ -9,43 +9,73 @@ toggleBtn.addEventListener("click", () => {
   }
 });
 
-const leftArrow = document.getElementById("left-arrow");
-const rightArrow = document.getElementById("right-arrow");
-const slideWrapper = document.querySelector(".content_inner_slider");
-
 const slides = document.querySelectorAll(".slide");
-const totalSlides = slides.length;
-console.log(totalSlides);
+const dots = document.querySelectorAll(".dot");
 let currentSlide = 0;
+let intervalId;
 
 function updateSlidePosition() {
-  for (let i = 0; i < totalSlides; i++) {
-    slides[i].style.transform = `translateX(-${currentSlide * 100}%)`;
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.toggle("active", i === currentSlide);
+    dots[i].classList.toggle("active", i === currentSlide);
   }
 }
-leftArrow.addEventListener("click", () => {
+
+function goToSlide(index) {
+  currentSlide = index;
+  updateSlidePosition();
+}
+
+function nextSlide() {
+  if (currentSlide < slides.length - 1) {
+    currentSlide++;
+  } else {
+    currentSlide = 0;
+  }
+  updateSlidePosition();
+}
+
+function prevSlide() {
   if (currentSlide > 0) {
     currentSlide--;
-    updateSlidePosition();
-    console.log("left clicked");
+  } else {
+    currentSlide = slides.length - 1;
   }
+  updateSlidePosition();
+}
+
+intervalId = setInterval(nextSlide, 3000);
+
+document
+  .querySelector(".carousel-wrapper")
+  .addEventListener("mouseover", () => {
+    clearInterval(intervalId);
+  });
+
+document.querySelector(".carousel-wrapper").addEventListener("mouseout", () => {
+  intervalId = setInterval(nextSlide, 3000);
 });
 
-rightArrow.addEventListener("click", () => {
-  if (currentSlide < totalSlides - 1) {
-    currentSlide++;
-    updateSlidePosition();
-    console.log("right clicked");
-  }
+dots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    goToSlide(index);
+    clearInterval(intervalId);
+  });
 });
+
+document.getElementById("right-arrow").addEventListener("click", () => {
+  clearInterval(intervalId);
+  nextSlide();
+});
+
+document.getElementById("left-arrow").addEventListener("click", () => {
+  clearInterval(intervalId);
+  prevSlide();
+});
+
+updateSlidePosition();
 
 const faqs = document.querySelectorAll(".faq");
-
-// faqs.forEach((faq) => {
-//   faq.addEventListener("click", () => {
-//     faq.classList.toggle("active");
-//   });
-// });
 
 faqs.forEach((listElement) => {
   listElement.addEventListener("click", () => {
