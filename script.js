0c26c1366926258fd14364d316389a8486f3656b
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2024-01-27 21:04:48 +0400

    accordion update(close when open one)

--- script.js
+++ script.js
@@ -9,38 +9,53 @@ toggleBtn.addEventListener("click", () => {
   }
 });
 
-// const leftArrow = document.getElementById("left-arrow");
-// const rightArrow = document.getElementById("right-arrow");
-// const slideWrapper = document.querySelector(".content_inner_slider");
+const leftArrow = document.getElementById("left-arrow");
+const rightArrow = document.getElementById("right-arrow");
+const slideWrapper = document.querySelector(".content_inner_slider");
 
-// const slides = document.getElementsByClassName("slide");
-// const totalSlides = slides.length;
-// console.log(totalSlides);
-// let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+const totalSlides = slides.length;
+console.log(totalSlides);
+let currentSlide = 0;
 
-// function updateSlidePosition() {
-//   for (let i = 0; i < totalSlides; i++) {
-//     slides[i].style.transform = `translateX(-${currentSlide * 100}%)`;
-//   }
-// }
-// leftArrow.addEventListener("click", () => {
-//   if (currentSlide > 0) {
-//     currentSlide--;
-//     updateSlidePosition();
-//   }
-// });
+function updateSlidePosition() {
+  for (let i = 0; i < totalSlides; i++) {
+    slides[i].style.transform = `translateX(-${currentSlide * 100}%)`;
+  }
+}
+leftArrow.addEventListener("click", () => {
+  if (currentSlide > 0) {
+    currentSlide--;
+    updateSlidePosition();
+    console.log("left clicked");
+  }
+});
 
-// rightArrow.addEventListener("click", () => {
-//   if (currentSlide < totalSlides - 1) {
-//     currentSlide++;
-//     updateSlidePosition();
-//   }
-// });
+rightArrow.addEventListener("click", () => {
+  if (currentSlide < totalSlides - 1) {
+    currentSlide++;
+    updateSlidePosition();
+    console.log("right clicked");
+  }
+});
 
 const faqs = document.querySelectorAll(".faq");
 
-faqs.forEach((faq) => {
-  faq.addEventListener("click", () => {
-    faq.classList.toggle("active");
+// faqs.forEach((faq) => {
+//   faq.addEventListener("click", () => {
+//     faq.classList.toggle("active");
+//   });
+// });
+
+faqs.forEach((listElement) => {
+  listElement.addEventListener("click", () => {
+    if (listElement.classList.contains("active")) {
+      listElement.classList.remove("active");
+    } else {
+      faqs.forEach((listE) => {
+        listE.classList.remove("active");
+      });
+      listElement.classList.toggle("active");
+    }
   });
 });
